cf1c4f7cef52ff53409fc3da8a8df7075f334d92
IvanKharkivskyi1 <ikcomua@gmail.com>
2024-12-21 00:01:56 +0200

    ping

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from '@chakra-ui/react';
 export const Calendar = () => {
   return (
     <Box flexGrow={1}>
-      <Text>&quot;CalendarPing&quot;</Text>
+      <Text>&quot;CalendarPingPing&quot;</Text>
     </Box>
   );
 };
